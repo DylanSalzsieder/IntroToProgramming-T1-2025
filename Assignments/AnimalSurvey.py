f43dcf0fb380ed16692fddd152560ba7081064be
Salzsieder Dylan <825743@stma.k12.mn.us>
2025-09-26 13:44:52 -0500

    animal survey

---
+++ Assignments/AnimalSurvey.py
@@ -0,0 +1,7 @@
+favorite_animal = input("What is your favorite animal?\n>")
+reason = input("Why do you like them?\n>")
+location = input("Where do they live?\n>")
+fun_fact = input("What is a fun fact about them?\n>")
+color = input("What color are they?\n>")
+
+print("Your favorite animal is the " + favorite_animal + " because " + reason + ". They live in " + location + ", and " + fun_fact + ". They are " + color)
