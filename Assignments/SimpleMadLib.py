79b25736fa67e83103414dde3bccfa9bf50c4cdd
Salzsieder Dylan <825743@stma.k12.mn.us>
2025-09-30 13:24:18 -0500

    simple mad lib

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,9 @@
+adjective1 = input("Enter an adjective\n> ")
+noun1 = input("Enter a noun\n> ")
+adjective2 = input("Enter another adjective\n> ")
+noun2 = input("Enter another noun\n> ")
+verb1 = input("Enter a verb ending in -ed\n> ")
+adverb1 = input("Enter an adverb ending in -ly\n> ")
+
+print("Once upon a time, there was a " + adjective1 + " " + noun1 + ".")
+print("Every day the " + noun1 + " walked up to the " + adjective2 + " " + noun2 + " and " + verb1 + " the " + noun2 + " " + adverb1 + ".")
